36a023ab93587a6a2e63c1d7eec1a338cf32bc58
prithaupadhyay <prithaupadhyay@gmail.com>
2019-01-25 20:56:55 +0530

    changed component name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { StudentComponent } from './student/student.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
-  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'student', component: StudentComponent },
   { path: 'heroes', component: HeroListComponent },
   { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
   { path: 'notFound', component: PageNotFoundComponent }
